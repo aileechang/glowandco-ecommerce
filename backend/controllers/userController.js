c6ced904abb5762d324fcfa4e85f563ef0186f06
aileechang <aileechang.swe@gmail.com>
2025-01-08 15:11:09 -0900

    Add user login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,11 +7,6 @@ const createToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET);
 }
 
-// Route for user login
-const loginUser = async (req, res) => {
-
-};
-
 // Route for user registration
 const registerUser = async (req, res) => {
     try {
@@ -45,6 +40,35 @@ const registerUser = async (req, res) => {
         const token = createToken(user.id);
 
         res.json({ success: true, token });
+
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message});
+    }
+};
+
+// Route for user login
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Check if user exists
+        const user = await User.findOne({ where: { email } });
+
+        if (!user) {
+            return res.json({ success: false, message: 'User does not exist' });
+        }
+
+        // Compare provided password with the hashed password
+        const isMatch = await bcrypt.compare(password, user.password);
+        
+        if (isMatch) {
+            // Generate a token
+            const token = createToken(user.id);
+            res.json({ success: true, token });
+        } else {
+            res.json({ success: false, message: 'Invalid credentials' });
+        }
 
     } catch (error) {
         console.log(error);
