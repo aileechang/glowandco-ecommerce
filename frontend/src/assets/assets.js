ea554812ac133dbad7e68b091b557e3177dd0b36
aileechang <aileechang.swe@gmail.com>
2025-01-10 23:09:39 -0900

    Initialize frontend and add assets

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,46 @@
+// Import products below for products array
+
+
+//Import assets
+
+
+// Export assets
+/* 
+
+export const assets = {
+    logo,
+}
+
+*/
+
+// Create mock product data
+/*
+
+export const products = [
+    {
+        _id: 'aaaaa',
+        name: '',
+        description: '',
+        price: 0,
+        image: [name_image],
+        category: '',
+        subCategory: '',
+        sizes: ['', '', ''],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: 'aaaab',
+        name: '',
+        description: '',
+        price: 0,
+        image: [name_image],
+        category: '',
+        subCategory: '',
+        sizes: ['', '', ''],
+        date: 1716621345448,
+        bestseller: true
+    },
+]
+
+*/
