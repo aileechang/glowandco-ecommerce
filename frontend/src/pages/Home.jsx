e60a8f95ae85726d150b31bd0db45d9102ac0e79
aileechang <aileechang.swe@gmail.com>
2025-01-16 23:45:41 -0900

    Add hero and new arrivals to homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import NewArrivals from '../components/NewArrivals'
 
 const Home = () => {
   return (
     <div>
-      
+      <Hero />
+      <NewArrivals />
     </div>
   )
 }
