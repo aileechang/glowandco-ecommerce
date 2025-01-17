c0a067ef79f1e20aa5552668207aa71890ed79e2
aileechang <aileechang.swe@gmail.com>
2025-01-17 00:10:10 -0900

    Add bestseller on homepage

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -7,7 +7,7 @@ const ProductItem = ({ id, image, name, price }) => {
   return (
     <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
       <div className='overflow-hidden'>
-        <img className='w-[170px] h-[230px] object-cover hover:scale-110 transition ease-in-out' src={image[0]} alt={name} />
+        <img className='w-[180px] h-[210px] object-cover hover:scale-110 transition ease-in-out' src={image[0]} alt={name} />
       </div>
       <p className='w-[170px] pt-3 pb-1 text-sm'>{name}</p>
       <p className='text-sm font-medium'>{currency}{price}</p>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import NewArrivals from '../components/NewArrivals'
+import BestSeller from '../components/BestSeller'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <NewArrivals />
+      <BestSeller />
     </div>
   )
 }
