766d07db316c8a6ed1a5315ea68309c430f8e434
aileechang <aileechang.swe@gmail.com>
2025-01-18 23:06:49 -0900

    Implement cart increment

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext  = createContext();
 
@@ -11,6 +12,11 @@ const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
 
     const addToCart = async (itemId, size) => {
+        if (!size) {
+            toast.error('Select product size');
+            return;
+        }
+
         let cartData = structuredClone(cartItems);
 
         if (cartData[itemId]) {
@@ -26,14 +32,28 @@ const ShopContextProvider = (props) => {
         setCartItems(cartData);
     }
 
-    useEffect(()=> {
-        console.log(cartItems);
-    }, [])
+    const getCartCount = () => {
+        let totalCount = 0;
+
+        for(const items in cartItems){
+            for(const item in cartItems[items]){
+                try {
+                    if (cartItems[items][item] > 0) {
+                        totalCount += cartItems[items][item];
+                    }
+                } catch (error) {
+                    
+                }
+            }
+        }
+        return totalCount;
+    }
     
     const value = {
         products, currency, delivery_fee,
         search, setSearch, showSearch, setShowSearch,
-        cartItems, addToCart
+        cartItems, addToCart,
+        getCartCount
     }
 
     return (
