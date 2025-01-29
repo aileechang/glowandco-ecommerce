e84492b3886aa89519cc577e696585316b6ed63e
aileechang <aileechang.swe@gmail.com>
2025-01-28 22:38:45 -0900

    Update hero styles

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -5,22 +5,32 @@ const Hero = () => {
   return (
     <div
       className={
-        "relative w-full h-[88vh] md:bg-cover md:bg-center bg-right md:bg-no-repeat flex items-center justify-left"
+        `relative w-full h-[88vh] md:bg-cover md:bg-center bg-right md:bg-no-repeat flex items-center justify-center md:justify-start`
       }
       style={{ backgroundImage: `url(${assets.hero})` }}
     >
-        <div className="w-full md:w-[450px] h-[300px] md:h-[500px] p-20">
-            <div className="flex flex-row gap-3 items-center ml-8">
-            <hr className="border-1 border-black w-[30px]"/>
-            <p className="cormorant-garamond-medium-italic text-5xl bg-none">NEW</p>
-            </div>
-            <p className="cormorant-garamond-bold text-6xl ml-20">ARRIVALS</p>
-            <div className="flex flex-row gap-3 items-center mt-10">
-            <p className="font-medium text-l tracking-wide">COLLECTION OUT NOW</p>
-            <hr className="border-1 border-black w-[70px]"/>
-            </div>
-            <button className="bg-black text-white mt-14 ml-20 px-8 py-3 hover:text-black hover:bg-white">SHOP HERE</button>
+      <div className="bg-white w-full md:w-[45%] m-10 md:m-15 py-8 px-4 flex justify-center md:justify-start rounded-xl">
+        <div className="w-full md:w-[5800px] flex flex-col items-center">
+          <div className="flex flex-row gap-3 items-center">
+            <hr className="border-1 border-black w-[30px]" />
+            <p className="cormorant-garamond-regular-italic text-slate-800 text-4xl bg-none">
+              NEW
+            </p>
+          </div>
+          <p className="cormorant-garamond-bold-italic text-[#727D71] text-6xl">
+            ARRIVALS
+          </p>
+          <div className="flex flex-row gap-3 items-center mt-1">
+            <p className="font-semibold text-md md:text-sm text-slate-600 tracking-wide">
+              LATEST COLLECTION HERE
+            </p>
+            <hr className="border-2 border-slate-600 w-[50px] md:w-[70px]" />
+          </div>
+          <button className="bg-[#727D71] text-white font-bold rounded-full  mt-16 px-8 py-3 hover:text-[#727D71] hover:bg-white">
+            SHOP NOW
+          </button>
         </div>
+      </div>
     </div>
   );
 };
